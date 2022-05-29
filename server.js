92c7b05ebce7db6dc7ec5e0bf0269fa9af801d9e
VuLong <vulongpt@gmail.com>
2022-05-29 16:06:19 +0700

    refactor code

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const User = require("./model/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const route = require("./routes");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const JWT_SECRET = "sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk";
 
-mongoose.connect(
-  "mongodb+srv://vulongpt:vulongpt@cluster0.utibo.mongodb.net/?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const app = express();
 app.use(bodyParser.json());
